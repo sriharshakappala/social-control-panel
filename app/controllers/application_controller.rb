7845152b1a14b9dda7c8e501d5c858d8def92757
vagrant <vagrant@precise32.(none)>
2015-01-15 08:49:59 +0000

    Moved twitter_init to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  client = Twitter::REST::Client.new do |config|
-	  config.consumer_key        = "VzDTU00oeY6X0YOQ0x1bf2dli"
-	  config.consumer_secret     = "BLn1nqFGFDXDKDac1yskkYm8iprx4IV3SUW2Dg80wK9kc1Ti5z"
-	  config.access_token        = "2976980166-s0rU6BKkoqYMSpDSjMpUzvuFmynfSiCOX4hdJdQ"
-	  config.access_token_secret = "k7iD5qJ031aIXYB38YjtZQwGG6KTJJo4HHa8I17P8I7kO"
-	end
-
   protected
 
   def after_sign_in_path_for(resource)
     dashboards_path
+  end
+
+  def twitter_init
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "VzDTU00oeY6X0YOQ0x1bf2dli"
+      config.consumer_secret     = "BLn1nqFGFDXDKDac1yskkYm8iprx4IV3SUW2Dg80wK9kc1Ti5z"
+      config.access_token        = "2976980166-s0rU6BKkoqYMSpDSjMpUzvuFmynfSiCOX4hdJdQ"
+      config.access_token_secret = "k7iD5qJ031aIXYB38YjtZQwGG6KTJJo4HHa8I17P8I7kO"
+    end
   end
 
 end
